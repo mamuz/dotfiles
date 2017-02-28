415d237e8fda6ef3fd8209b011434ba6ba9c1859
mamuz <mamuz.de@gmail.com>
2017-02-28 14:01:40 +0100

    optimizations

--- brew.sh
+++ brew.sh
@@ -20,7 +20,9 @@ brew install curl wget --with-iri
 brew install vim --with-override-system-vi
 brew install homebrew/dupes/grep homebrew/dupes/openssh homebrew/dupes/screen
 brew install mtr git-lfs tree tmux jmeter jq python awscli terminal-notifier
-brew install docker docker-machine docker-compose
+
+brew install docker docker-machine
+brew cask install docker-compose
 
 brew cask install google-chrome
 brew cask install firefox
