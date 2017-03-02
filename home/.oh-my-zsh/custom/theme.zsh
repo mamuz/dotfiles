c49caee0102aa83dc2666cb7ed7fb1fef99ffa00
mamuz <mamuz.de@gmail.com>
2017-03-02 02:59:26 +0100

    optimizations

--- home/.oh-my-zsh/custom/theme.zsh
+++ home/.oh-my-zsh/custom/theme.zsh
@@ -1,4 +1,4 @@
-zsh_display_awsenv() {
+zsh_custom_aws() {
     if [ -n "$AWS_DEFAULT_PROFILE" ]; then
         echo -n "$AWS_DEFAULT_PROFILE";
     fi
@@ -10,7 +10,7 @@ zsh_display_awsenv() {
 ZSH_THEME="powerlevel9k/powerlevel9k"
 
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status zsh_display_awsenv time)
-POWERLEVEL9K_CUSTOM_AWS="zsh_display_awsenv"
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status custom_aws time)
+POWERLEVEL9K_CUSTOM_AWS="zsh_custom_aws"
 POWERLEVEL9K_CUSTOM_AWS_FOREGROUND="007"
 POWERLEVEL9K_CUSTOM_AWS_BACKGROUND="166"
