67c354c907ec220d9e18571366e1b948d450ece6
mamuz <mamuz.de@gmail.com>
2017-03-09 16:05:41 +0100

    add docker cask

--- install/brew.sh
+++ install/brew.sh
@@ -7,12 +7,12 @@ brew upgrade
 brew install brew-cask
 brew upgrade brew-cask
 
-brew install coreutils moreutils findutils
+brew install coreutils moreutils findutils htop-osx
 brew install gnu-sed --with-default-names
 brew install zsh zsh-completions
 brew tap homebrew/versions
 
-brew install htop-osx curl wget --with-iri
+brew install curl wget --with-iri
 brew install vim --with-override-system-vi
 brew install homebrew/dupes/grep homebrew/dupes/openssh homebrew/dupes/screen
 brew install mtr git-lfs tree tmux jmeter jq python awscli terminal-notifier ctags
