317f1a7a42b5f289289995fda5f8264fb5f0a4f8
mamuz <mamuz.de@gmail.com>
2017-04-20 11:12:18 +0200

    add phptools

---
+++ home/.oh-my-zsh/custom/phptools.zsh
@@ -0,0 +1,97 @@
+phpda() {
+    tty=
+    tty -s && tty=--tty
+    docker run \
+       $tty \
+       --interactive \
+       --rm \
+       --user $(id -u):$(id -g) \
+       --volume /etc/passwd:/etc/passwd:ro \
+       --volume /etc/group:/etc/group:ro \
+       --volume $(pwd):/app \
+       mamuz/php-analyzer phpda "$@"
+}
+
+phpcs() {
+    tty=
+    tty -s && tty=--tty
+    docker run \
+       $tty \
+       --interactive \
+       --rm \
+       --user $(id -u):$(id -g) \
+       --volume /etc/passwd:/etc/passwd:ro \
+       --volume /etc/group:/etc/group:ro \
+       --volume $(pwd):/app \
+       mamuz/php-analyzer phpcs "$@"
+}
+
+phpmd() {
+    tty=
+    tty -s && tty=--tty
+    docker run \
+       $tty \
+       --interactive \
+       --rm \
+       --user $(id -u):$(id -g) \
+       --volume /etc/passwd:/etc/passwd:ro \
+       --volume /etc/group:/etc/group:ro \
+       --volume $(pwd):/app \
+       mamuz/php-analyzer phpmd "$@"
+}
+
+phpcpd() {
+    tty=
+    tty -s && tty=--tty
+    docker run \
+       $tty \
+       --interactive \
+       --rm \
+       --user $(id -u):$(id -g) \
+       --volume /etc/passwd:/etc/passwd:ro \
+       --volume /etc/group:/etc/group:ro \
+       --volume $(pwd):/app \
+       mamuz/php-analyzer phpcpd "$@"
+}
+
+phploc() {
+    tty=
+    tty -s && tty=--tty
+    docker run \
+       $tty \
+       --interactive \
+       --rm \
+       --user $(id -u):$(id -g) \
+       --volume /etc/passwd:/etc/passwd:ro \
+       --volume /etc/group:/etc/group:ro \
+       --volume $(pwd):/app \
+       mamuz/php-analyzer phploc "$@"
+}
+
+security-check() {
+    tty=
+    tty -s && tty=--tty
+    docker run \
+       $tty \
+       --interactive \
+       --rm \
+       --user $(id -u):$(id -g) \
+       --volume /etc/passwd:/etc/passwd:ro \
+       --volume /etc/group:/etc/group:ro \
+       --volume $(pwd):/app \
+       mamuz/php-analyzer security-checker "$@"
+}
+
+phpmetrics() {
+    tty=
+    tty -s && tty=--tty
+    docker run \
+       $tty \
+       --interactive \
+       --rm \
+       --user $(id -u):$(id -g) \
+       --volume /etc/passwd:/etc/passwd:ro \
+       --volume /etc/group:/etc/group:ro \
+       --volume $(pwd):/app \
+       mamuz/php-analyzer phpmetrics "$@"
+}
