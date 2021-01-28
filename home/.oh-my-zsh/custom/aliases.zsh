8c6ae4808362ef9390f9bddfa6ecde0f0bf63c86
Marco Muths <mamuz.de@gmail.com>
2021-01-28 14:31:01 +0100

    Update aliases.zsh

--- home/.oh-my-zsh/custom/aliases.zsh
+++ home/.oh-my-zsh/custom/aliases.zsh
@@ -17,31 +17,16 @@ alias mkdir='mkdir -pv'
 alias week='date +%V'
 
 # Get Software Updates
-if [[ "$OSTYPE" = darwin* ]]; then
-    alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cask list | xargs brew cask install --force; brew cleanup; brew cask cleanup; sudo gem update --system; sudo gem update; sudo gem cleanup'
-else
-    alias update='sudo apt-get clean && sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove'
-fi
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cask list | xargs brew cask install --force; brew cleanup; brew cask cleanup; sudo gem update --system; sudo gem update; sudo gem cleanup'
 
 # Empty the Trash on all mounted volumes and the main HDD.
-if [[ "$OSTYPE" = darwin* ]]; then
-    alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
-else
-    alias emptytrash='sudo rm -rf /home/*/.local/share/Trash/*/* >/dev/null 2>&1'
-fi
+alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
 
 # Lock the screen (when going AFK)
-if [[ "$OSTYPE" = darwin* ]]; then
-    alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
-else
-    alias afk="gnome-screensaver-command -l"
-fi
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
 
 # Reload the shell (i.e. invoke as a login shell)
 alias reload="exec $SHELL -l"
-
-# Hub alias to git
-eval "$(hub alias -s)"
 
 # Shortcuts
 alias www='cd $HOME/Projects'
