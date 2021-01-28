6e014a724b0901a24b9ecafd6b867cd62bf2311e
Marco Muths <mamuz.de@gmail.com>
2021-01-28 14:24:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,7 @@
 
 git pull origin master
 mkdir ~/.ssh/
-
-if [[ "$OSTYPE" = darwin* ]]; then
-    source ./install/brew.sh
-else
-    source ./install/apt.sh
-fi
+source ./install/brew.sh
 
 if [ ! -f $HOME/.oh-my-zsh/oh-my-zsh.sh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -24,10 +19,6 @@ vim +PluginUpdate +PluginClean +qall
 if [ ! -f $HOME/.vim/colors/solarized.vim ]; then
     mkdir -p $HOME/.vim/colors
     cp $HOME/.vim/bundle/vim-colors-solarized/colors/solarized.vim $HOME/.vim/colors/
-fi
-
-if [[ "$OSTYPE" = darwin* ]]; then
-    source ./home/.macos
 fi
 
 echo "Done."
