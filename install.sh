504d01ca63a4a5421751c8d2fdf6dd4c3ab26dd0
Marco Muths <mamuz.de@gmail.com>
2021-01-28 14:38:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git pull origin master
-mkdir ~/.ssh/
+mkdir $HOME/.ssh/
 
 sudo softwareupdate -i -a
 
@@ -24,14 +24,14 @@ fi
 
 if [ ! -f $HOME/.oh-my-zsh/oh-my-zsh.sh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+    git clone https://github.com/bhilburn/powerlevel9k.git $HOME/.oh-my-zsh/custom/themes/powerlevel9k
 fi
 
 if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 fi
 
-cp -R -i ./home/.* ~
+cp -R -i ./home/.* $HOME
 vim +PluginUpdate +PluginClean +qall
 
 if [ ! -f $HOME/.vim/colors/solarized.vim ]; then
